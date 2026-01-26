19c9d299372290659fa47ae4912e6244eac19737
farzia <ghaforyfarzia@gmail.com>
2026-01-26 12:06:22 +0100

    Add package structure, empty classes, and ignore out folder

--- src/main/java/es/fplumara/dam1/actividades/model/Usuario.java
+++ src/main/java/es/fplumara/dam1/actividades/model/Usuario.java
@@ -1,4 +1,19 @@
 package es.fplumara.dam1.actividades.model;
 
 public class Usuario {
+    private Long id;
+    private String nombre;
+    private String perfil;
+    private String discordUserId;
+    private String curso;
+    private String email;
+    public Usuario(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
