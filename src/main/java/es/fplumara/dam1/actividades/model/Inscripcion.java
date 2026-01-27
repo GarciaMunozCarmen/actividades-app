83b82b92be986d6096cdad11c4a0453d788e22dc
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-27 14:08:34 +0100

    dominio y implement UsuarioRepository

--- src/main/java/es/fplumara/dam1/actividades/model/Inscripcion.java
+++ src/main/java/es/fplumara/dam1/actividades/model/Inscripcion.java
@@ -1,4 +1,53 @@
 package es.fplumara.dam1.actividades.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class Inscripcion {
+    private UUID id;
+    private UUID idTaller;
+    private UUID idUsuario;
+    private RolInscripcion rol;
+    private LocalDateTime fechaInscripcion;
+    private static final String FORMAT_DATE_TIME = "dd-MM-yyyy / HH:mm:ss";
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getIdTaller() {
+        return idTaller;
+    }
+
+    public void setIdTaller(UUID idTaller) {
+        this.idTaller = idTaller;
+    }
+
+    public UUID getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(UUID idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public RolInscripcion getRol() {
+        return rol;
+    }
+
+    public void setRol(RolInscripcion rol) {
+        this.rol = rol;
+    }
+
+    public LocalDateTime getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public void setFechaInscripcion(LocalDateTime fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
 }
--- src/main/java/es/fplumara/dam1/actividades/model/Taller.java
+++ src/main/java/es/fplumara/dam1/actividades/model/Taller.java
@@ -1,4 +1,70 @@
 package es.fplumara.dam1.actividades.model;
 
+import java.net.URL;
+import java.util.UUID;
+
 public class Taller {
+    private UUID id;
+    private String titulo;
+    private String descripcion;
+    private EstadoInscripcion estadoInscripcion;
+    private URL url; //He investigado y no sé si podríamos poner esto o String
+    private int cupo;
+    private String lugar;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public EstadoInscripcion getEstadoInscripcion() {
+        return estadoInscripcion;
+    }
+
+    public void setEstadoInscripcion(EstadoInscripcion estadoInscripcion) {
+        this.estadoInscripcion = estadoInscripcion;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public int getCupo() {
+        return cupo;
+    }
+
+    public void setCupo(int cupo) {
+        this.cupo = cupo;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
 }
